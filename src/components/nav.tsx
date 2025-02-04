b8242251f4236aa827eaef0d4f8936ca70e760f1
9rw <9rw9rw9@gmail.com>
2025-02-05 01:29:24 +0700

    update navbar

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import Image from "next/image";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
-import { Search, Globe } from "lucide-react";
+import { Search, Globe, Menu } from "lucide-react";
 import { link } from "fs";
 
 export default function Nav() {
@@ -12,19 +12,20 @@ export default function Nav() {
       <div className="min-w-[200px] min-h-[100px] max-h-[100px] max-w-[200px]">
         <Image
           src="/logo.svg"
-          width={200}
-          height={100}
+          width={0}
+          height={0}
           alt={"Logo"}
           priority={true}
           className="w-full h-full object-fill"
         ></Image>
       </div>
-      <div className="flex flex-col max-w-[600px] w-full">
+      <div className="flex flex-col w-full">
         <div className="flex flex-row justify-between items-center bg-white border-2 border-[#0d3168dd] rounded-lg w-full h-[50px]">
           <Input
+            name="search"
             type="search"
             placeholder="SEARCH FOR ACTIVITIES"
-            className="bg-white border-0 shadow-[0] max-w-[600px] w-full h-full"
+            className="bg-white border-0 shadow-[0] w-full h-full"
           ></Input>
           <Button variant={"outline"} className="h-full border-0">
             <Search />
@@ -42,13 +43,22 @@ export default function Nav() {
           ))}
         </div>
       </div>
-      <div className="relative w-full h-[100px] flex items-start flex-row">
+      <div className="relative w-max h-[100px] mt-[10px] flex items-start flex-row">
+        <div className="flex flex-row">
+          <Button variant={"link"}>
+            <Globe />
+          </Button>
+          <Button variant={"link"}>EN</Button>
+          <hr className="h-[36px] w-[1px] bg-black" />
+          <Button variant={"link"}>TH</Button>
+        </div>
+        <div className="flex flex-row gap-2">
+          <Button variant={"secondary"}>Help</Button>
+          <Button variant={"secondary"}>Report</Button>
+        </div>
         <Button variant={"link"}>
-          <Globe />
+          <Menu />
         </Button>
-        <Button variant={"link"}>EN</Button>
-        <hr className="h-[36px] w-[1px] bg-black" />
-        <Button variant={"link"}>TH</Button>
       </div>
     </nav>
   );
