05ff8d47f53158bf4b9e0889b403b1bd947bf34f
tik-04 <natthaphan0074@gmail.com>
2025-02-06 22:56:01 +0700

    Help

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -9,6 +9,15 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover"
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+  DialogClose
+} from "@/components/ui/dialog"
 
 export default function Nav() {
   return (
@@ -56,9 +65,71 @@ export default function Nav() {
           <hr className="h-[36px] w-[1px] bg-black" />
           <Button variant={"link"}>TH</Button>
         </div>
+        
+        {/* Help Button with dialog */}
         <div className="flex flex-row gap-2 max-lg:hidden">
-          <Button variant={"secondary"}>Help</Button>
-          <Button variant={"secondary"}>Report</Button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant={"secondary"}>Help</Button>
+          </DialogTrigger>
+          <DialogContent className="max-w-2xl p-10 max-h-[90vh] overflow-y-auto">
+            <DialogHeader>
+              <div className="min-w-[200px] min-h-[100px] max-h-[100px] max-w-[200px] max-sm:min-w-[50px] max-sm:min-h-[50px] max-sm:max-h-[50px]">
+                <Image
+                  src="/logo.svg"
+                  width={219}
+                  height={112}
+                  alt={"Logo"}
+                  priority={true}
+                  className="w-full h-full object-fill"
+                ></Image>
+
+              </div>
+            </DialogHeader>
+            <DialogTitle>
+              <h1 className=" font-normal text-2xl mt-3 text-[#1B2845]">Help</h1>
+            </DialogTitle>
+            <DialogDescription>
+            <h2 className="mt-4 text-xl text-[#1B2845]">
+              How to log in ？
+            </h2>
+            <div className="text-gray-700 text-sm leading-relaxed max-h-[400px] overflow-y-auto mt-4">
+              <p className="text-sm text-[#1B2845] indent-10">
+                  Those who have the right to access the room reservation system must be employees within the Faculty only, using the same login and password as SIT-mail if the person wishing to reserve a room is a Faculty student. Please contact your Educational Services Officer or Student Developer.
+              </p>
+            </div>
+            <h2 className="mt-4 text-xl text-[#1B2845]">
+            Why can't I delete or change a booking ？
+            </h2>
+            <div className="text-gray-700 text-sm leading-relaxed max-h-[400px] overflow-y-auto mt-4">
+              <p className="text-sm text-[#1B2845] indent-10">
+              To delete or change a room reservation Only the person who created the booking has the right to delete or change their booking. You cannot delete or change someone else's booking.
+              </p>
+            </div>
+
+            <h2 className="mt-4 text-xl text-[#1B2845]">What are the different colors when booking a room ?</h2>
+            <div className="mt-5 flex flex-row ">
+              <Button className=" w-[117px] h-[40px] bg-[#9D64A7] border-[1px] border-solid border-black ">Lecturer</Button>
+              <Button className=" w-[154px] h-[40px] bg-[#D988B9] border-[1px] border-solid border-black ml-2">Student / Staff</Button>
+            </div>
+
+            <h2 className="mt-4 text-xl text-[#1B2845]">How can I view the booking schedule ?</h2>
+            <p className="text-sm text-[#1B2845] indent-10 mt-4">  Looking at the room booking time, each channel is 30 minutes, for example, booking a room for 1 hours is 8:30-10:00 a.m. The highlighted colors cover 4 channels as shown in the picture.</p>
+            <div className="mt-3 flex flex-row">
+            <Image
+                  src="/Help column.webp"
+                  width={886}
+                  height={169}
+                  alt={"Logo"}
+                  priority={true}
+                  className="w-full h-full object-fill"
+                ></Image>
+            </div>
+            </DialogDescription>
+          </DialogContent>
+        </Dialog>
+
+        <Button variant={"secondary"}>Report</Button>
         </div>
         <Popover>
         <PopoverTrigger asChild>
