dbbf91ca852bd9be47df53b4dc09e0199ff33e03
9rw <9rw9rw9@gmail.com>
2025-02-07 12:59:55 +0700

    update

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -110,7 +110,7 @@ export default function Hero() {
                 role="combobox"
                 aria-expanded={openArea}
                 aria-label="Select Building Area"
-                className="w-[200px] justify-between"
+                className="w-[200px] justify-between uppercase"
               >
                 {value
                   ? areas.find((area) => area.value === value)?.value
