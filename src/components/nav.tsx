12a0fa5d569df01341201ce242e0a5c7c55d602f
9rw <9rw9rw9@gmail.com>
2025-02-08 19:48:46 +0700

    +

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -14,7 +14,7 @@ import {
 export default function Nav() {
   const [openHelp, setOpenHelp] = useState(false);
   return (
-    <nav className="z-[999] fixed top-0 flex justify-between items-center px-10 py-4 w-full h-[164px] bg-white text-primary-background border-b-2 border-primary-foreground/20 shadow-lg">
+    <nav className="z-[49] fixed top-0 flex justify-between items-center px-10 py-4 w-full h-[164px] bg-white text-primary-background border-b-2 border-primary-foreground/20 shadow-lg">
       <div className="min-w-[200px] min-h-[100px] max-h-[100px] max-w-[200px] max-sm:min-w-[50px] max-sm:min-h-[50px] max-sm:max-h-[50px]">
         <Link href={`/`}>
           <Image
